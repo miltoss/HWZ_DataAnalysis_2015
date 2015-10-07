6b44837439cc25f153dc2bd41054d3f9ce175f9d
miltoss <m.sarakinos@yahoo.com>
2015-10-07 21:36:00 +0200

    Simple tutorial to learn vectors in R

--- vectors.R
+++ vectors.R
@@ -38,17 +38,27 @@ s
 
 # Here are other ways to initialise a vector:
 
-x <- 1:5
-y <- rep(2,5)
+x <- 10:20
+y <- rep(2,10)
 
 # Let's get the 3rd element of x
 x[3]
+# or just the first three elements:
+x[1:3]
+# do it in a different way:
+head(x, 3)
+# default is 6 elements
+head(x)
+# or the last elements
+tail(x)
 
 
 # We can add vectors
 x + y
-# We can multiply vectors
+# We can multiply vectors 
+# (vectors elements multiplied elementwise resulting in a vector)
 x * y
+
 
 # We can combine (concatenate) vectors to build a larger vector:
 c(x, y)
