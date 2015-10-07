0b92572a42bb2c54acf057932a754dde018ae34d
miltoss <m.sarakinos@yahoo.com>
2015-10-07 20:51:04 +0200

    Second Commit

--- distributions.R
+++ distributions.R
@@ -1,4 +1,5 @@
 
+
 # Set window to 2-column graph
 par(mfrow = c(1, 2))
 
@@ -27,6 +28,13 @@ hist(x, xlim = c(mu - 5 * sigma, mu + 5 * sigma))
 # Chisq distribution: needs parameter df (degress of freedom). 
 # There is no default value. Try with df = 1, 2, 3 
 # Try to put several on the same graph (add = T when calling curve or hist)
+# The distribution is chisq in R (analog unif, norm)
 
 
 
+
+
+
+
+# Set window back to 1 x 1 column graph
+par(mfrow = c(1, 2))
