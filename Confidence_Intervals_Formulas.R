5a92f485bcb4fd7874a11cdf0e0b9ccb34731151
miltoss <m.sarakinos@yahoo.com>
2015-10-15 20:56:45 +0200

    Confidence Intervals

--- Confidence_Intervals_Formulas.R
+++ Confidence_Intervals_Formulas.R
@@ -1,8 +1,16 @@
 
-conf.int.z <- function(xbar, S, n, alpha) {
+# Define a function for the confidence interval for estimating the mean of a normal distribution
+# when we have a sample of n data points and we know the mean sigma of the universe
+conf.int.z <- function(xbar, sigma, n, alpha) {
     
-    d <- qnorm(1 - alpha / 2) * S / sqrt(n) 
+    d <- qnorm(1 - alpha / 2) * sigma / sqrt(n) 
     
+    cat("With", 1-alpha, "certainty the mean mu lies between", xbar, "+/-", d,  "\n")
+    cat("With", 1-alpha, "certainty the mean mu lies between", xbar -d, "and", xbar + d, "\n")
+
     return(d)
     
 }
+
+# Test the function
+conf.int.z(xbar = 10, sigma = 2, n = 4, alpha = 0.05)
