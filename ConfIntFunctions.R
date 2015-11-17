db451addf5ee7672b54260ce99d76ffa7e71ec5f
miltoss <m.sarakinos@yahoo.com>
2015-11-17 20:17:57 +0100

    Functions for confidence interval calculation from formulas

--- ConfIntFunctions.R
+++ ConfIntFunctions.R
@@ -1,5 +1,6 @@
+
 conf.int.z <- function(xbar, sigma, n, alpha) {
-    
+    # Calculates confidence interval from formula when sigma is known.    
     d <- qnorm(1 - alpha / 2) * sigma / sqrt(n) 
     
     cat("With", 1-alpha, "certainty the mean mu lies between", xbar, "+/-", d,  "\n")
@@ -10,6 +11,7 @@ conf.int.z <- function(xbar, sigma, n, alpha) {
 }
 
 conf.int.t <- function(xbar, s, n, alpha) {
+    # Calculates confidence interval from formula when sigma is unknown.
     
     d <- qt(1 - alpha / 2, df = n - 1) * s / sqrt(n) 
     
@@ -23,7 +25,7 @@ conf.int.t <- function(xbar, s, n, alpha) {
 
 
 n.int.z <- function(d, xbar, sigma, alpha) {
-    
+    # Calculates the necessary sample size to achieve a confidence interval when sigma is unknown
     n <- qnorm(1 - alpha / 2) * sigma / d
     n <- n**2
     return(n)
@@ -33,6 +35,7 @@ n.int.z(d = 1.95, xbar = 2.5, sigma = 2, alpha = 0.05)
 
 
 conf.int.prop <- function(p, n, alpha) {
+    # Calculates the confidence interval for a proportion.
     d <- qnorm(1 - alpha / 2) * sqrt(p * (1-p) / n)
     cat("With", 1-alpha, "the proportion lies between", p, "+/-", d,  "\n")
     cat("With", 1-alpha, "the proportion lies between", p -d, "and", p + d, "\n")
